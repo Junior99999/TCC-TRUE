ecf74febadfad6935d876e5416c5a02fa2c913d0
Junior99999 <juniormachdo11@gmail.com>
2022-02-01 09:04:33 -0300

    Usando ejs

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser')
 var path = require('path')
 
+//CONFIGURAÇÃO DO SISTEMA\\
+
 app.use(cookieParser())
 
 app.use(bodyParser.json())
@@ -13,8 +15,17 @@ app.set("view engine", "ejs")
 
 app.use(express.static(path.join(__dirname,"public")))
 
+//ROTAS PARA EJS\\
+
 app.get('/', function(req,res){
-    res.send('Olá mundo!')
+    res.render('index.ejs',{})
+})
+
+app.get('/usuarios', function(req,res){
+    res.render('usuarios.ejs',{usuarios:[
+        {nome:'Junior',email:'juniormachdo11@gmail.com'},
+        {nome:'Adm',email:'adm@gmail.com'}
+    ]})
 })
 
 app.listen(3000, function(){
