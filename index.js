5a17373d895b86505aa03f77b4f2017d65a3f4c7
Junior99999 <juniormachdo11@gmail.com>
2022-02-02 09:28:10 -0300

    Terminei

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser')
 var path = require('path')
 var Usuario = require ('./model/usuario')
+const { redirect } = require('express/lib/response')
+var upload = require('./config/configMulter')
 
 //CONFIGURAÇÃO DO SISTEMA\\
 
@@ -19,22 +21,30 @@ app.use(express.static(path.join(__dirname,"public")))
 //ROTAS PARA EJS\\
 
 app.get('/', function(req,res){
-    res.render('index.ejs',{})
+    Usuario.find({}).exec(function(err,docs){
+        res.render('index.ejs',{Usuarios:docs})
+    })
+    
+})
+app.post('/', function(req,res){
+    Usuario.find({nome: new RegExp(req.body.txtPesquisa,'gi')}).exec(function(err,docs){
+            res.render('index.ejs', {Usuarios: docs})
+    })    
 })
 
-
+///////////////////////////////////
 //ADICIONAR\\
 
 app.get('/add', function(req,res){
     res.render('adiciona.ejs')
 })
 
-app.post('/add', function(req,res){
+app.post('/add',upload.single("txtFoto") ,function(req,res){
     var usuario = new Usuario({
         nome: req.body.txtNome,
         email: req.body.txtEmail,
         senha: req.body.txtSenha,
-        foto: req.body.txtFoto
+        foto: req.file.filename
     })
     usuario.save(function(err){
         if(err){
@@ -44,8 +54,39 @@ app.post('/add', function(req,res){
         }
     })
 })
-
 /////////////////////////////////////
+//DELETAR\\
+app.get('/del/:id',function(req,res){
+    Usuario.findByIdAndDelete(req.params.id, function(err){
+        if(err){
+            console.log(err)
+        }else{
+            res.redirect('/')
+        }
+    })
+})
+////////////////////////////////////
+//EDITAR\\
+app.get('/edit/:id', function(req,res){
+    Usuario.findById(req.params.id, function(err,docs){
+        if(err){
+            console.log(err)
+        }else{
+            res.render('edita.ejs',{Usuario: docs})
+        } 
+    })
+})
+
+app.post('/edit/:id',upload.single("txtFoto") ,function(req,res){
+    Usuario.findByIdAndUpdate(req.params.id,{
+        nome: req.body.txtNome,
+        email: req.body.txtEmail,
+        senha: req.body.txtSenha,
+        foto: req.file.filename
+    },function(err,docs){
+        res.redirect('/')
+    })
+})
 
 app.listen(3000, function(){
     console.log("Conexão inicializada na porta 3000")
