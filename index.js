7327d570a195708dd2a07d4560cc4458f20dcbdf
Junior99999 <juniormachdo11@gmail.com>
2022-02-23 09:44:09 -0300

    23022022

--- index.js
+++ index.js
@@ -55,8 +55,6 @@ app.post('/', function(req,res){
 
 ///////////////////////////////////
 //ADICIONAR\\
-
-
 app.get('/add', function(req,res){
     res.render('adiciona.ejs')
 })
@@ -76,6 +74,7 @@ app.post('/add',upload.single("txtFoto") ,function(req,res){
         }
     })
 })
+
 /////////////////////////////////////
 //DELETAR\\
 app.get('/del/:id',function(req,res){
@@ -88,6 +87,7 @@ app.get('/del/:id',function(req,res){
     })
 })
 ////////////////////////////////////
+
 //EDITAR\\
 app.get('/edit/:id', function(req,res){
     Usuario.findById(req.params.id, function(err,docs){
@@ -109,6 +109,7 @@ app.post('/edit/:id',upload.single("txtFoto") ,function(req,res){
         res.redirect('/')
     })
 })
+
 /////////////////////
 //CADASTRO USUÁRIO\\
 app.get('/cadastro', function(req,res){
@@ -159,6 +160,7 @@ app.post('/cadastro', async (req,res) =>{
         console.log('Aconteceu um erro')
     }
 })
+
 ///////////////////
 //LOGIN USUÁRIO\\
 app.get('/login', function(req,res){
@@ -190,6 +192,83 @@ app.post('/login', async (req,res) =>{
     try{
         return console.log('Logado com sucesso')
     }catch(Error){
+        console.log('Aconteceu um erro')
+    }
+})
+
+//AMD LOGIN//
+app.get('/admlogin', function(req,res){
+    res.render('./adm/loginAdm.ejs')
+})
+app.post('/admlogin', async (req,res) =>{
+    const {admEmail, admSenha} = req.body
+
+    //Validações
+    if(!admEmail){
+        return console.log('O email é Obrigatório')
+    }if(!admSenha){
+        return console.log('A senha é Obrigatório')
+    }
+    //checar se existe o usuário
+    const user = await Adm.findOne({ email: admEmail})
+    if(!user){
+        return console.log('Usuário não encontrado')
+    }
+
+    //checar se a senha confirma
+    const checarSenha = await bcrypt.compare(admSenha, user.senha)
+
+    if(!checarSenha){
+        return console.log('Senha inválida')
+    }
+    try{
+        return console.log('Logado com sucesso')
+    }catch(Error){
+        console.log('Aconteceu um erro')
+    }
+})
+
+//ADM CADASTRO//
+app.get('/admcadastro', function(req,res){
+    res.render('./adm/cadastroAdm.ejs')
+})
+app.post('/admcadastro', async (req,res) =>{
+
+    const {admNome, admEmail, admSenha, admConfirmasenha} = req.body
+
+    //Validações
+    if(!admNome){
+        return console.log('O nome é Obrigatório')
+    }if(!admEmail){
+        return console.log('O email é Obrigatório')
+    }if(!admSenha){
+        return console.log('A senha é Obrigatório')
+    }
+
+    if(admSenha !== admConfirmasenha){
+        return console.log('A senhas não conferem')
+    }
+
+    const userExist  = await Adm.findOne({ email: admEmail})
+    
+    if(userExist){
+        return console.log('Utilize outro email!')
+    }
+
+    const salt = await bcrypt.genSalt(12)
+    const senhaHash = await bcrypt.hash(admSenha, salt)
+
+    const user = new Adm({
+        nome: req.body.admNome,
+        email: req.body.admEmail,
+        senha: senhaHash,
+    })
+    try{
+        await user.save()
+        console.log('Registrado com sucesso')
+        return res.redirect('/')
+        
+    }catch(error){
         console.log('Aconteceu um erro')
     }
 })
