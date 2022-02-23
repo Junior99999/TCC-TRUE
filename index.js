b3594d6738f7f626cc203d2befcc0f51243bfa21
Junior99999 <juniormachdo11@gmail.com>
2022-02-23 09:54:43 -0300

    qqqqqqqqqqqq

--- index.js
+++ index.js
@@ -271,7 +271,7 @@ app.post('/admcadastro', async (req,res) =>{
     }catch(error){
         console.log('Aconteceu um erro')
     } 
-})
+})   
 app.listen(3000, function(){
     console.log("Conexão inicializada na porta 3000")
     }  
